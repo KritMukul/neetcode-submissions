0f99f0d551ae5a87542ec2e31fde0d5cf9008c13
Krit Mukul <142911721+KritMukul@users.noreply.github.com>
2026-07-12 16:05:47 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0], prof = 0;
+        for(int i = 1; i < prices.size(); i++) {
+            prof = max(prof, prices[i] - mini);
+            mini = min(mini, prices[i]);
+        }
+        return prof;
+    }
+};
