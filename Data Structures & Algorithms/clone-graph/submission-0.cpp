cb7b7d95d379c25db9e81a97da409fa8946cc1a3
Krit Mukul <142911721+KritMukul@users.noreply.github.com>
2026-08-30 16:12:28 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node) return nullptr;
+
+        unordered_map<Node*, Node*> mpp;
+        queue<Node*> q;
+        mpp[node] = new Node(node->val);
+        q.push(node);
+
+        while(!q.empty()) {
+            Node* curr = q.front();
+            q.pop();
+
+            for(Node* nei : curr->neighbors) {
+                if(mpp.find(nei) == mpp.end()) {
+                    mpp[nei] = new Node(nei->val);
+                    q.push(nei);
+                }
+                mpp[curr]->neighbors.push_back(mpp[nei]);
+            }
+        }
+        return mpp[node];
+    }
+};
