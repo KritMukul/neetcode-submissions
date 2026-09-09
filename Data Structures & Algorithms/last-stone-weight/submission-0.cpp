aadacf8c448ae09fe28eee1316aebbfd2096bf28
Krit Mukul <142911721+KritMukul@users.noreply.github.com>
2026-09-09 16:51:59 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end());
+
+        while(pq.size() >= 2) {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            if(x == y) continue;
+            else pq.push(abs(x-y));
+        }
+        return pq.size() ? pq.top(): 0;
+    }
+};
